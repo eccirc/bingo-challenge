a278b29a912583472f59896fbc54c2f45ce3f0a3
eccirc <davidgw@proton.me>
2023-11-26 15:57:56 +0000

    add basic get endpoint

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,3 +1,16 @@
 import express from "express"
 
-        const me = () => "you"
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get("/isBingo", (req,res) => {
+    console.log(req),
+    res.send({
+        isBingo: true
+    })
+})
+
+app.listen(port, () => {
+    console.log("Listening on port: " + port)
+})
